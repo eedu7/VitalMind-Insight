c777c3f73ddb9c104a0b0fdfb8afe9e9da8b50f5
eedu7 <mueedahmad067@gmail.com>
2025-08-30 09:35:29 +0500

    refactor(dependencies): replace direct UserService and UserCRUD instantiation with ServiceContainer in get_current_active_user

--- backend/core/dependencies/current_active_user.py
+++ backend/core/dependencies/current_active_user.py
@@ -1,14 +1,15 @@
 from fastapi import Depends, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from crud import UserCRUD
+from core.dependencies import services
 from db import get_session
 from db.models.user import User
 from services import UserService
 
-crud = UserCRUD()
-user_service = UserService(crud)
 
-
-async def get_current_active_user(request: Request, session: AsyncSession = Depends(get_session)) -> User:
+async def get_current_active_user(
+    request: Request,
+    user_service: UserService = Depends(services.get_user_service),
+    session: AsyncSession = Depends(get_session),
+) -> User:
     return await user_service.get_by_uuid(request.user.uuid, session)
