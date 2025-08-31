0c01c5d0ab114d36cb802432f7e43a0b6a5a9226
eedu7 <mueedahmad067@gmail.com>
2025-08-31 15:57:31 +0500

    Added types and scheams and useAuth

---
+++ frontend/src/features/auth/types.ts
@@ -0,0 +1,5 @@
+import z from "zod";
+import { loginFormSchema, registerFormSchema } from "./schemas";
+
+export type LoginFormValues = z.infer<typeof loginFormSchema>;
+export type RegisterFormValues = z.infer<typeof registerFormSchema>;
