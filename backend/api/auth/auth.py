463534c79175ac04e51bb1bf6786eba8a9744821
eedu7 <mueedahmad067@gmail.com>
2025-09-05 09:39:55 +0500

    feat(auth): add logout API with token blacklist support
    - Implemented `/logout` endpoint
    - Accepts access and refresh tokens for invalidation
    - Uses JWT handler to decode and validate tokens
    - Adds both tokens to the token blacklist for security
    - Applied rate limiting (2 requests/minute) to prevent abuse

--- backend/api/auth/auth.py
+++ backend/api/auth/auth.py
@@ -3,8 +3,9 @@ from fastapi_limiter.depends import RateLimiter
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.dependencies import services
+from core.security import jwt_handler, token_blacklist
 from db import get_session
-from schemas.auth import AuthLogin, AuthOut, AuthRegister
+from schemas.auth import AuthLogin, AuthLogOut, AuthOut, AuthRegister
 from services import AuthService
 
 router = APIRouter()
@@ -41,3 +42,20 @@ async def login(
     session: AsyncSession = Depends(get_session),
 ):
     return await auth_service.login(email=data.email, password=data.password, session=session)
+
+
+@router.post(
+    "/logout",
+    status_code=status.HTTP_200_OK,
+    dependencies=[Depends(RateLimiter(times=2, minutes=1))],
+)
+async def logout(
+    data: AuthLogOut,
+):
+    access_payload = jwt_handler.decode(data.access_token, expected_type="access")
+    refresh_payload = jwt_handler.decode(data.refresh_token, expected_type="refresh")
+
+    await token_blacklist.add(access_payload)
+    await token_blacklist.add(refresh_payload)
+
+    return {"message": "Successfully logged out."}
